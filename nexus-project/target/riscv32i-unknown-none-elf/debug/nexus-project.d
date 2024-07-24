2d4b84414a24ec2e57281587fbcef94eb3410e2c
0xlittleboy web3 <gobeyond707@gmail.com>
2024-07-24 17:06:38 +0000

    NexusProofv2

---
+++ nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project.d
@@ -0,0 +1,1 @@
+/workspace/NexusZkevm2/nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project: /workspace/NexusZkevm2/nexus-project/src/main.rs
---
+++ nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project.d
@@ -0,0 +1,1 @@
+/workspace/NexusZkevm2/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project: /workspace/NexusZkevm2/nexus-project/src/main.rs
